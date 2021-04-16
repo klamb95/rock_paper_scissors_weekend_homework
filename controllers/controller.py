d47082d296b9c79ebe4d74dfb79098e78e562b4c
klamb95 <klamb1995@gmail.com>
2021-04-16 15:40:38 +0100

    working MVP

--- controllers/controller.py
+++ controllers/controller.py
@@ -9,8 +9,3 @@ def index(choice_1, choice_2):
     return render_template('index.html', winner = winner)
 
 
-#below works!
-# @app.route("/<choice_1>/<choice_2>")
-# def index(choice_1, choice_2):
-#     winner = this_works(choice_1,choice_2)
-#     return render_template('index.html', winner = winner)
--- models/game.py
+++ models/game.py
@@ -1,4 +1,4 @@
-from models.player import * 
+from models.player import *
 
 
 player_1 = Player("Kieran", "Rock")
@@ -8,19 +8,20 @@ player_3 = Player("Nick", "Paper")
 
 players = [player_1, player_2]
 
+
 def get_preferred_option(player_1, player_2):
     if player_1.gesture == "Rock" and player_2.gesture == "Scissors":
         return player_1
-        
+
     if player_1.gesture == "Scissors" and player_2.gesture == "Paper":
         return player_1
-        
+
     if player_1.gesture == "Paper" and player_2.gesture == "Rock":
         return player_1
-    
+
     if player_1.gesture == player_2.gesture:
         return "draw"
-        
+
     return player_2
 
 
@@ -29,46 +30,30 @@ def get_this_working(gesture_1, gesture_2):
     gesture_2 = player_2
     if player_1.gesture == "Rock" and player_2.gesture == "Scissors":
         return player_1
-        
+
     if player_1.gesture == "Scissors" and player_2.gesture == "Paper":
         return player_1
-        
+
     if player_1.gesture == "Paper" and player_2.gesture == "Rock":
         return player_1
-    
+
     if player_1.gesture == player_2.gesture:
         return "draw"
-        
+
     return player_2
 
 
-
 def this_works(gesture_1, gesture_2):
-    if gesture_1 == "Rock" and gesture_2 == "Scissors":
-        gesture_1 = player_1
-        gesture_2 = player_2
+    if gesture_1 == "rock" and gesture_2 == "scissors":
         return " player 1!"
-        
-    if gesture_1 == "Scissors" and gesture_2 == "Paper":
-        gesture_1 = player_2
-        gesture_2 = player_3
+
+    if gesture_1 == "scissors" and gesture_2 == "paper":
         return " player 1!"
-        
-    if gesture_1 == "Paper" and gesture_2 == "Rock":
-        gesture_1 = player_3
-        gesture_2 = player_1
+
+    if gesture_1 == "paper" and gesture_2 == "rock":
         return " player 1!"
-    
+
     if gesture_1 == gesture_2:
-        return "draw"
-        
+        return "none"
+
     return ' player 2!'
-
-
-
-
-       
-
-       
-
-
