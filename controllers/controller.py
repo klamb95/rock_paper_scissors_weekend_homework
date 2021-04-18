2fd3cd528610324cab33b2486d0a6c56adddbe1a
klamb95 <klamb1995@gmail.com>
2021-04-18 15:04:39 +0100

    added basic CSS

--- controllers/controller.py
+++ controllers/controller.py
@@ -6,6 +6,6 @@ from models.game import get_preferred_option, get_this_working, this_works
 @app.route('/<choice_1>/<choice_2>')
 def index(choice_1, choice_2):
     winner = this_works(choice_1,choice_2)
-    return render_template('index.html', winner = winner)
+    return render_template('index.html', title="Home", winner = winner)
 
 
